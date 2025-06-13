0627cd3a4f3ad0de3b220a6792ffc387c424827b
huytq <huyquangtr1105@gmail.com>
2025-06-13 10:17:53 +0700

    first commit

---
+++ android/app/src/main/kotlin/com/example/demo_custom_painter/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.demo_custom_painter
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,9 @@
+import 'package:demo_custom_painter/src/lets_draw_app.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const LetsDrawApp());
+}
+
+const Color kCanvasColor = Color(0xfff2f3f7);
+const String kGithubRepo = 'https://github.com/JideGuru/flutter_drawing_board';
